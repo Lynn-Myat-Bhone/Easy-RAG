e04e24f19e33a8de8e7a3c2044766a793da40843
Lynn-Myat-Bhone <lynnmyatbhone311@gmail.com>
2026-04-04 14:38:35 +0630

    for csv

--- RAG/loader.py
+++ RAG/loader.py
@@ -1,21 +1,27 @@
-import json
+import csv
 from langchain_core.documents import Document
 
-def load_json(data_path):
-    with open(data_path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        
+def load_csv(data_path):
     documents = []
-    
-    for item in data:
-        text = f""" {item['question']}
-{item['answer']}"""
-        
-        documents.append(
-            Document(
-                page_content=text,
-                metadata={"lang": "my"}
+
+    with open(data_path, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            text = f"{row['question_mm']}\n{row['answer_mm']}"
+            metadata = {
+                "id": row["id"],
+                "topic": row["topic"],
+                "difficulty": row["difficulty"],
+                "lang": "my",
+                "source": row["source_file"]
+            }
+
+            documents.append(
+                Document(
+                    page_content=text.strip(),
+                    metadata=metadata
+                )
             )
-        )
-        
+
     return documents
